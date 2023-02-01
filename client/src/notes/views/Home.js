6788aa12fd6bfe36faa028aa6b4f621d6764614a
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2023-02-01 00:10:32 -0500

    bootstrap added

--- client/src/notes/views/Home.js
+++ client/src/notes/views/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 const Home = () => {
     return (
         <div>
             <p className='homeTitle'>Just click one of the buttons below pls</p>
-            <Link to="/login" className='loginLink'>Login</Link>
-            <Link to="/register">Register</Link>
+            <div className="homeButtons">
+                <Link to="/login"><button>Login</button></Link>
+                <Link to="/register">Register</Link>
+
+                
+            </div>
+
+            <Button variant="primary">Primary</Button>
         </div>
     )
 }
